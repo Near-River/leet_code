2d7d68e66b2eb2d2a7ef574737fb6b091ee4f631
Near_River <798563420@qq.com>
2016-07-29 13:35:48 +0800

    day_001 update--2

--- 01_10/09_palindrome_number.py
+++ 01_10/09_palindrome_number.py
@@ -12,12 +12,25 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # if x < 0: return False
+        # return x == int(str(x)[::-1])
+
         if x < 0: return False
-        return x == int(str(x)[::-1])
+        if x < 10: return True
+        base = 1
+        while x // base > 10:
+            base *= 10
+        while x:
+            left, right = x // base, x % 10
+            if left != right: return False
+            x -= base * left
+            x //= 10
+            base //= 100
+        return True
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.isPalindrome(103))
+    # print(solution.isPalindrome(103))
     print(solution.isPalindrome(12321))
     print(solution.isPalindrome(-43234))
